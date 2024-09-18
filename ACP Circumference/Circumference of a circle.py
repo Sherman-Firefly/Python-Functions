b48c6f835d191a5283002af47e167b54da48acb2
Sherman-Firefly <efracw2007@gmail.com>
2024-09-18 17:56:39 +0800

    Circumference

---
+++ ACP Circumference/Circumference of a circle.py
@@ -0,0 +1,6 @@
+def cir(r):
+    return 2*3.14*r
+
+print("The circumference of a circle is found with the forumla 2 * pi * radius")
+rad=int(input("input the radius"))
+print("Circumference of a circle is", cir(rad))
